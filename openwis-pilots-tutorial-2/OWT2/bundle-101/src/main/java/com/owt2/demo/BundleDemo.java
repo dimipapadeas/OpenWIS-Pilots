0c91bcdf4cf9f000cce8fc5d82ebb870b9772951
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-08-03 12:07:55 +0300

    Adding OWT2 code

---
+++ openwis-pilots-tutorial-2/OWT2/bundle-101/src/main/java/com/owt2/demo/BundleDemo.java
@@ -0,0 +1,14 @@
+package com.owt2.demo;
+
+
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+
+@Singleton
+public class BundleDemo {
+    @PostConstruct
+    public void init() {
+        System.out.println("A new Bundle has started!");
+    }
+
+}
